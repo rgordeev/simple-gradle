093b68e9b868f830ac189c362dcd12e40b4203a9
rgordeev <roma.gordeev@gmail.com>
2019-10-24 20:41:56 +0300

    solve task 1.4.6

---
+++ src/main/java/ru/rgordeev/tasks14/Task6.java
@@ -0,0 +1,11 @@
+package ru.rgordeev.tasks14;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a & (a - 1));
+    }
+}
